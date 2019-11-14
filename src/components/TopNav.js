5b485424c7636e5b26224f1b30f866c364d7f309
HytonightYX <whosiyuan@qq.com>
2019-11-14 15:40:24 +0800

    feat: 设置路由, 分离路由设置到 config

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,28 +1,35 @@
 import { Container, Menu } from 'semantic-ui-react'
 import IVC_LOGO from '../assert/IVC_LOGO.png'
 import React from 'react'
+import { Link, withRouter } from 'react-router-dom'
+import { MENU } from '../constant/config'
 
-const TopNav = ({fixed}) => (
-	<Menu
-		fixed={fixed ? 'top' : null}
-		inverted={!fixed}
-		pointing={!fixed}
-		secondary={!fixed}
-		size='large'
-	>
-		<Container>
-			<Menu.Item position='left'>
-				<img src={IVC_LOGO} alt=""/>
-			</Menu.Item>
+const TopNav = withRouter(({fixed, location}) => {
 
-			<Menu.Item as='a' active>
-				主页
-			</Menu.Item>
-			<Menu.Item as='a'>成果</Menu.Item>
-			<Menu.Item as='a'>联系我们</Menu.Item>
-			<Menu.Item as='a'>关于我们</Menu.Item>
-		</Container>
-	</Menu>
-)
+	return (
+		<Menu
+			fixed={fixed ? 'top' : null}
+			inverted={!fixed}
+			pointing={!fixed}
+			secondary={!fixed}
+			size='large'
+		>
+			<Container>
+				<Menu.Item position='left'>
+					<img src={IVC_LOGO} alt=""/>
+				</Menu.Item>
+				{
+					MENU.map((item) => (
+						<Link to={item.path}>
+							<Menu.Item as='a' key={item.name} active={location.pathname === item.path}>
+								{item.name}
+							</Menu.Item>
+						</Link>
+					))
+				}
+			</Container>
+		</Menu>
+	)
+})
 
 export default TopNav
---
+++ src/constant/config.js
@@ -0,0 +1,6 @@
+export const MENU = [
+	{name: '主页',    path: '/'},
+	{name: '成果',    path: '/achievement'},
+	{name: '联系我们', path: '/contact'},
+	{name: '关于我们', path: '/about'},
+]
