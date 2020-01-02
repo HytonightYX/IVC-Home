96d43b2dfaf2edab10ecab52471b8e5cfdbeffba
HytonightYX <whosiyuan@qq.com>
2020-01-02 19:33:04 +0800

    feat: 数据库请求 member 信息

--- client/src/pages/about/index.js
+++ client/src/pages/about/index.js
@@ -1,14 +1,35 @@
-import { Modal as AntModal } from 'antd'
 import { Modal } from 'semantic-ui-react'
 import React, { Component } from 'react'
+import axios from 'axios'
 import './style.less'
 import IMG_DJC from '../../assert/member/djc.png'
-import { IVC_MEMBER } from '../../constant/config'
+import { API_MEMBER_LIST } from '../../constant/api'
 
 class About extends Component {
 
 	state = {
+		memberList: [],
+		loading: false,
 		visible: false
+	}
+
+	componentDidMount() {
+		this.setState({loading: true})
+		axios.get(API_MEMBER_LIST)
+			.then((data) => {
+				const r = data.data
+				console.log(r.code === 200)
+				if (r && r.code === 200) {
+					this.setState({
+						memberList: r.data,
+						loading: false
+					})
+				}
+			})
+			.catch(e => {
+				console.log(e)
+				this.setState({loading: false})
+			})
 	}
 
 	showModal = () => {
@@ -26,6 +47,9 @@ class About extends Component {
 
 	render() {
 
+		const {memberList} = this.state
+		console.log(memberList)
+
 		return (
 			<div className="g-about">
 				<div className="m-title">
@@ -34,22 +58,23 @@ class About extends Component {
 
 				<div className="m-content">
 					{
-						IVC_MEMBER.map((item, index) => {
+						memberList.map((item) => {
 							return (
-								<div className="member-card">
+								<div className="member-card" key={item.id}>
 									<div className="member-img">
-										<img src={IMG_DJC} alt=""/>
+										<img src={item.image || IMG_DJC} alt=""/>
 									</div>
 									<div className="member-ct">
 										<div className="name">{item.name}</div>
 
-										<div className="grade">{item.grade}</div>
+										<div className="grade">{item.tag}</div>
 
-										<Modal trigger={<div className="more">more ></div>} closeIcon centered={false} className="m-member-modal">
+										<Modal trigger={<div className="more">more ></div>} closeIcon centered={false}
+										       className="m-member-modal">
 											<Modal.Content>
 												<div className="m-image"><img src={IMG_DJC} alt=""/></div>
 												<div className="m-name">{item.name}</div>
-												<div className="m-desc">{item.description}</div>
+												<div className="m-desc">{item.bio}</div>
 											</Modal.Content>
 										</Modal>
 									</div>
@@ -69,8 +94,6 @@ class About extends Component {
 				{/*	<p>Some contents...</p>*/}
 				{/*	<p>Some contents...</p>*/}
 				{/*</Modal>*/}
-
-
 			</div>
 		)
 	}
